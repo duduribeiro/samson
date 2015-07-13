3c9b580937ea5867d2c151c774856bdf71ae3126
Shane Hender <henders@gmail.com>
2015-07-13 15:32:44 +0100

    method override for buddy checks

--- plugins/pipelines/app/decorators/stage_decorator.rb
+++ plugins/pipelines/app/decorators/stage_decorator.rb
@@ -6,4 +6,11 @@ Stage.class_eval do
     stages = project.stages.to_a
     stages[stages.index(self) + 1]
   end
+
+  alias_method :old_production?, :production?
+  # Return true if any stages in the pipeline are marked production
+  def production?
+    return old_production? if next_stage_ids.empty?
+    old_production? || Stage.find(job.deploy.stage.next_stage_ids).any?(:old_production?)
+  end
 end
