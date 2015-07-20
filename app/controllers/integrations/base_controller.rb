200abe5d8819ceb35516fd37dbc6f2e00302afe1
Shane Hender <henders@gmail.com>
2015-07-20 16:28:16 +0100

    Trigger docker build creation when webhooks are triggered.

--- app/controllers/integrations/base_controller.rb
+++ app/controllers/integrations/base_controller.rb
@@ -3,31 +3,15 @@ class Integrations::BaseController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def create
-    return head :ok unless deploy?
-    project.deploy_with_docker? ? create_docker_build : create_release_and_deploy
+    return head(:ok) if !deploy?
+    if project.create_releases_for_branch?(branch)
+      create_build_record
+      create_docker_image if project.deploy_with_docker? && project.auto_release_docker_image?
+    end
+    deploy_to_stages ? head(:ok) : head(:unprocessable_entity, message: "Failed to start all deploys")
   end
 
   protected
-
-  def create_new_release
-    if project.create_releases_for_branch?(branch)
-      if project.last_release_contains_commit?(commit)
-        project.releases.order(:id).last
-      else
-        release_service = ReleaseService.new(project)
-        release_service.create_release!(commit: commit, author: user)
-      end
-    end
-  end
-
-  def deploy_to_stages
-    stages = project.webhook_stages_for(branch, service_type, service_name)
-    deploy_service = DeployService.new(user)
-
-    stages.all? do |stage|
-      deploy_service.deploy!(stage, reference: commit).persisted?
-    end
-  end
 
   def project
     @project ||= Project.find_by_token!(params[:token])
@@ -40,10 +24,16 @@ class Integrations::BaseController < ApplicationController
   end
 
   def user
-    name = self.class.name.split("::").last.sub("Controller", "")
-    email = "deploy+#{name.underscore}@#{Rails.application.config.samson.email.sender_domain}"
+    @user ||= begin
+      name = self.class.name.split("::").last.sub("Controller", "")
+      email = "deploy+#{name.underscore}@#{Rails.application.config.samson.email.sender_domain}"
 
-    User.create_with(name: name, integration: true).find_or_create_by(email: email)
+      User.create_with(name: name, integration: true).find_or_create_by(email: email)
+    end
+  end
+
+  def message
+    ''
   end
 
   private
@@ -56,24 +46,38 @@ class Integrations::BaseController < ApplicationController
     @service_name ||= self.class.name.demodulize.sub('Controller', '').downcase
   end
 
-  def create_docker_build
-    release = create_new_release
-    build = project.builds.create(
+  def create_build_record
+    release = create_new_release || latest_release
+    @build = project.builds.create!(
       git_ref: branch,
       git_sha: commit,
       description: message,
       creator: user,
-      label: release.version)
-    build.persisted? ? head(:ok) : head(:bad_request, message: build.errors)
+      label: release.version,
+      releases: [release])
   end
 
-  def create_release_and_deploy
-    create_new_release
-    return head :unprocessable_entity unless deploy_to_stages
-    head :ok
+  def create_docker_image
+    DockerBuilderService.new(@build).run!(push: true)
   end
 
-  def message
-    ''
+  def deploy_to_stages
+    stages = project.webhook_stages_for(branch, service_type, service_name)
+    deploy_service = DeployService.new(user)
+
+    stages.all? do |stage|
+      deploy_service.deploy!(stage, reference: commit).persisted?
+    end
+  end
+
+  def create_new_release
+    unless project.last_release_contains_commit?(commit)
+      release_service = ReleaseService.new(project)
+      release_service.create_release!(commit: commit, author: user)
+    end
+  end
+
+  def latest_release
+    project.releases.order(:id).last
   end
 end
